bc8883094c1480d3c7a7312b9466dba8b2cd1cec
Xavier Graells <fxgraells@outlook.com>
2023-10-27 11:59:36 +0200

    Feat: Added AWS S3 bucket URL for seeder database sample

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,17 +1,16 @@
 import { Coffee } from '../context/coffeeTypes';
 import '../styles/Card.css';
-import blackCup from '../assets/blackCup.png';
-import clearCup from '../assets/clearCup.png';
+
 interface CardProps {
   coffee: Coffee;
 }
 
 const Card: React.FC<CardProps> = ({ coffee }) => {
-  console.log('pathImg', coffee.imageUrl);
+  console.log('URL-Img:', coffee.imageUrl);
   return (
     <div className="coffee-card">
       <div>{coffee.type}</div>
-      <img src={blackCup} alt="cupPic" />
+      <img src={coffee.imageUrl} alt="cupPic" />
       <p>{coffee.title}</p>
       <p>{coffee.description}</p>
       <p>{coffee.price}</p>
